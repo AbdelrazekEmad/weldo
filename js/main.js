04ca2e9274031214bbfa747edb10e5791a29eb02
Abdelrazek Emad <53626803+AbdelrazekEmad@users.noreply.github.com>
2022-12-21 15:53:58 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -38,3 +38,30 @@ $(".owl-carousel-testimonial .owl-dot span").css({
   width: "20px",
   height: "5px",
 });
+
+jQuery("#animated-thumbnails-gallery")
+  .justifiedGallery({
+    captions: false,
+    lastRow: "hide",
+    rowHeight: 180,
+    margins: 5,
+    download: false,
+  })
+  .on("jg.complete", function () {
+    window.lightGallery(
+      document.getElementById("animated-thumbnails-gallery"),
+      {
+        autoplayFirstVideo: false,
+        pager: false,
+        galleryId: "nature",
+        plugins: [lgThumbnail],
+        download: false,
+        mobileSettings: {
+          controls: false,
+          showCloseIcon: false,
+          download: false,
+          rotate: false,
+        },
+      }
+    );
+  });
